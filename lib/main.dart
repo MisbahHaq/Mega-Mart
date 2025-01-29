4283c7385e8cdb49335b2361a3d888729daa243d
Misbah Haque <misbahmuneer2003@gmail.com>
2025-01-29 12:36:30 +0500

    Firebase Integrated

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyBA_aureSlJHC9PjrKdiOJVgkPM_4oUFzE",
-      authDomain: "mega-mart-30566.firebaseapp.com",
-      projectId: "mega-mart-30566",
-      storageBucket: "mega-mart-30566.firebasestorage.app",
-      messagingSenderId: "1094641258892",
-      appId: "1:1094641258892:web:b64513308bed5422e386a9",
-      measurementId: "G-NTDPQD4PG2",
-    ),
-  );
+      // options: FirebaseOptions(
+      //   apiKey: "AIzaSyBA_aureSlJHC9PjrKdiOJVgkPM_4oUFzE",
+      //   authDomain: "mega-mart-30566.firebaseapp.com",
+      //   projectId: "mega-mart-30566",
+      //   storageBucket: "mega-mart-30566.firebasestorage.app",
+      //   messagingSenderId: "1094641258892",
+      //   appId: "1:1094641258892:web:b64513308bed5422e386a9",
+      //   measurementId: "G-NTDPQD4PG2",
+      // ),
+      );
 
   runApp(const MyApp());
 }
