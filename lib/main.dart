9c148e3155dc816bcdfaf3be2ca1109b452500a0
Misbah Haque <misbahmuneer2003@gmail.com>
2025-01-31 11:30:16 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,7 @@ import 'package:megamart/Pages/profile.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-      // options: FirebaseOptions(
-      //   apiKey: "AIzaSyBA_aureSlJHC9PjrKdiOJVgkPM_4oUFzE",
-      //   authDomain: "mega-mart-30566.firebaseapp.com",
-      //   projectId: "mega-mart-30566",
-      //   storageBucket: "mega-mart-30566.firebasestorage.app",
-      //   messagingSenderId: "1094641258892",
-      //   appId: "1:1094641258892:web:b64513308bed5422e386a9",
-      //   measurementId: "G-NTDPQD4PG2",
-      // ),
-      );
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
